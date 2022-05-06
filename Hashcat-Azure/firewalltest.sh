57eeb1e7030f24817fd40c6f9c4d611639eabd97
ramondunker <r.dunker@st.hanze.nl>
2022-05-06 16:35:25 +0200

    test

--- Hashcat-Azure/firewalltest.sh
+++ Hashcat-Azure/firewalltest.sh
@@ -14,10 +14,10 @@ done
 # apt-get -o DPkg::Lock::Timeout=60 -y install ufw
 
 # Hardening
-# ufw --force enable
+ufw --force enable
 echo $allowed_ip > /root/allowed_ip.txt
-# ufw allow from $allowed_ip
-# ufw allow to $allowed_ip
+ufw allow from $allowed_ip
+ufw allow to $allowed_ip
 
 # Reboot (recommended by CUDA installers and to enable UFW)
 # reboot
